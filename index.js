aafcf4b00215f890ec9ef6f14f44c35dbb1b5e7e
dave-mp <davidajmp922@gmail.com>
2023-06-10 23:43:17 -0500

    Exercises 3.1-3.6

---
+++ index.js
@@ -0,0 +1,92 @@
+import express, { json } from "express";
+const app = express();
+
+app.use(json());
+
+let persons = [
+  {
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/info", (request, response) => {
+  response.send(`
+  <p>Phonebook has info for ${persons.length} people</p>
+  <p>${new Date().toString()}</p>
+  `);
+});
+
+app.get("/api/persons", (request, response) => {
+  response.json(persons);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = +request.params.id;
+  const person = persons.find((person) => person.id === id);
+  if (person) {
+    response.json(person);
+  } else {
+    response.statusMessage = "The person doesn't exist";
+    response.status(404).end();
+  }
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = persons.length > 0 ? Math.max(...persons.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/persons", (request, response) => {
+  const { name, number } = request.body;
+
+  const errors = [];
+
+  if (!name) errors.push("name is missing");
+  if (!number) errors.push("number is missing");
+  if (name && persons.find((p) => p.name.toLowerCase() === name.toLowerCase()))
+    errors.push("name must be unique");
+
+  if (errors.length > 0)
+    return response.status(400).json({
+      errors,
+    });
+
+  const person = {
+    name,
+    number,
+    id: generateId(),
+  };
+
+  persons = persons.concat(person);
+
+  response.json(person);
+});
